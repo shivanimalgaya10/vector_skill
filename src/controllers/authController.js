a96ff38fcd3e15a0999b78c71c4b0c59485918aa
shivanimalgaya10 <123302341+shivanimalgaya10@users.noreply.github.com>
2025-11-13 20:58:23 +0530

    main chnages

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,8 +4,9 @@ const User = require('../models/User');
 
 const register = async (req, res, next) => {
   try {
+
     const { name, email, password } = req.body;
-    const existing = await User.findOne({ email });
+    const existing = await User.findOne({ email,password });
     if (existing) return res.status(400).json({ message: 'User already exists' });
     const hashed = await bcrypt.hash(password, 10);
     const user = await User.create({ name, email, password: hashed, role });
